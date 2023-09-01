ae052a26f7302f8809a47cbaf687894c6eb0d896
Daniel D Bautista <eindal.bautista@gmail.com>
2023-09-01 09:12:29 +0800

    client socket

---
+++ botclient.py
@@ -0,0 +1,6 @@
+from networking.client import Client
+import networking.messages as msg
+
+client = Client()
+client.connect()
+client.disconnect()
---
+++ networking/client.py
@@ -0,0 +1,30 @@
+import socket
+import networking.messages as msg
+import os
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+PORT = int(os.getenv('SERVER_PORT'))
+CODEC = os.getenv('CODEC')
+HEADER_LEN = int(os.getenv('HEADER_LEN'))
+SERVER_IP = os.getenv('SERVER_IP')
+
+class Client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        self.client.connect((SERVER_IP, PORT))
+
+    def disconnect(self):
+        self.send(msg.DISCONNECT)
+        response = self.receive()
+        print(response)
+
+    def send(self, message: str):
+        message = json.dumps(message)
+        self.client.send(message.encode(CODEC))
+    
+    def receive(self):
+        message = self.client.recv(HEADER_LEN).decode(CODEC)
+        return message
