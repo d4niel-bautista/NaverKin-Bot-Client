26d428fe11759aa2ce0d647b09860385462e3868
Daniel D Bautista <eindal.bautista@gmail.com>
2023-09-04 16:42:25 +0800

    added functions for updating accounts and questions. disconnect function can update account status to 0(available)

--- networking/messages.py
+++ networking/messages.py
@@ -1,4 +1,6 @@
 DISCONNECT = {'message': 'DISCONNECT'}
 GET_ID = {'message': 'GET_ACCOUNT'}
 GET_QUESTION = {'message': 'GET_QUESTION', 'data': {'username': ''}}
+UPDATE_ACCOUNT = {'message': 'UPDATE_ACCOUNT', 'data': {'username': '', 'status': ''}}
+UPDATE_QUESTION = {'message': 'UPDATE_QUESTION', 'data': {'id': '', 'respondent': '', 'status': ''}}
 ID_REACHED_LIMIT = {'message': 'ID_REACHED_LIMIT'}
--- networking/service.py
+++ networking/service.py
@@ -20,5 +20,28 @@ class Service():
         response = self.client.receive()
         return response
     
-    def disconnect(self):
+    def update_question(self, question_id, username):
+        request = msg.UPDATE_QUESTION
+        request['data']['id'] = question_id
+        request['data']['respondent'] = username
+        print(request)
+        self.client.send(request)
+        response = self.client.receive()
+        return response
+    
+    def update_account(self, username, status):
+        request = msg.UPDATE_ACCOUNT
+        request['data']['username'] = username
+        request['data']['status'] = status
+        self.client.send(request)
+        response = self.client.receive()
+        return response
+    
+    def disconnect(self, username):
+        request = msg.UPDATE_ACCOUNT
+        request['data']['username'] = username
+        request['data']['status'] = 0
+        self.client.send(request)
+        response = self.client.receive()
         self.client.disconnect()
+        return response
