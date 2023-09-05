81764545867c3c4c95626c41c79de5a31fab5d1e
Daniel D Bautista <eindal.bautista@gmail.com>
2023-09-05 15:30:07 +0800

    refactored load cookies function by grouping cookies by domain

--- crawler/session_manager.py
+++ crawler/session_manager.py
@@ -1,15 +1,21 @@
 import time
 
 def load_cookies(driver, cookies):
+    grouped_cookies = {}
     for cookie in cookies:
-        for key, val in cookie.items():
-            if key == 'domain':
-                if val[0] == '.':
-                    val = val[1:]
-                driver.get('https://' + val)
-                time.sleep(5)
-                driver.add_cookie(cookie)
-                break
+        if cookie['domain'][0] == '.':
+            domain = cookie['domain'][1:]
+        else:
+            domain = cookie['domain']
+        if not domain in grouped_cookies.keys():
+            grouped_cookies[domain] = []
+        grouped_cookies[domain].append(cookie)
+    for i in grouped_cookies:
+        driver.get('https://' + i)
+        time.sleep(2)
+        for j in grouped_cookies[i]:
+            driver.add_cookie(j)
+        time.sleep(5)
 
 def load_useragent(options, useragent):
     if useragent:
