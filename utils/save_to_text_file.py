951928fc30e05dd731f1fd9d181fc48399d13eed
Daniel D Bautista <eindal.bautista@gmail.com>
2023-09-07 11:07:06 +0800

    fixed saving to text file by writing as binary with euc-kr encoding

--- utils/save_to_text_file.py
+++ utils/save_to_text_file.py
@@ -1,11 +1,11 @@
 import os
 
-def save_to_text_file(filename, directory, content):
+def save_to_text_file(filename, directory, content: str):
     if not os.path.isdir(directory):
         os.makedirs(directory, exist_ok=True)
     filepath = os.path.join(directory, filename)
     if not os.path.isfile(filepath):
-        with open(filepath, 'w+') as f:
+        with open(filepath, 'wb+') as f:
             f.close()
-    with open(filepath, 'w+') as f:
-            f.write(content)
+    with open(filepath, 'wb+') as f:
+            f.write(content.rstrip().encode('euc-kr'))
