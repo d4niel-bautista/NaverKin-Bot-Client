ba84c2f78cadc5fe4553445829a3c1b94e4079c5
Daniel D Bautista <eindal.bautista@gmail.com>
2023-09-08 11:02:35 +0800

    refactored if-statement on get_all_windows to filter Google Chrome windows first. changed pyautogui to use win32com.client to send 'alt' key

--- utils/focus_browser_window.py
+++ utils/focus_browser_window.py
@@ -1,13 +1,13 @@
-import win32gui
-import pyautogui
+import win32gui, win32com.client
 import time
 
 def get_all_windows(window_name_keywords):
     result = []
     def winEnumHandler(hwnd, ctx):
         for keyword in window_name_keywords:
-            if win32gui.IsWindowVisible(hwnd) and 'Google Chrome' in win32gui.GetWindowText(hwnd) and win32gui.GetWindowText(hwnd) == keyword or keyword in win32gui.GetWindowText(hwnd):
-                result.append(hwnd)
+            if win32gui.IsWindowVisible(hwnd) and 'Google Chrome' in win32gui.GetWindowText(hwnd):
+                if win32gui.GetWindowText(hwnd) == keyword or keyword in win32gui.GetWindowText(hwnd):
+                    result.append(hwnd)
     win32gui.EnumWindows(winEnumHandler, None)
     return result
 
@@ -18,6 +18,6 @@ def bring_browser_to_front(window_name_keywords=['지식iN', 'Naver Sign in', 'K
             try:
                 win32gui.SetForegroundWindow(hwnd)
             except:
-                pyautogui.press("alt")
+                win32com.client.Dispatch("WScript.Shell").SendKeys('%')
                 win32gui.SetForegroundWindow(hwnd)
             time.sleep(1)
