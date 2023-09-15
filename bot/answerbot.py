ad51227badd66b7935599bf29d22e83252747810
Daniel D Bautista <eindal.bautista@gmail.com>
2023-09-15 15:30:15 +0800

    updated AnswerBot main program flow

--- bot/answerbot.py
+++ bot/answerbot.py
@@ -51,8 +51,6 @@ class AnswerBot(NaverKinBot):
             if not self.can_interact(question['author']):
                 time.sleep(self.cooldown)
                 self.release_account()
-                if self.stop:
-                    return
                 return self.start()
             if self.stop:
                 return
@@ -71,34 +69,42 @@ class AnswerBot(NaverKinBot):
             pyautogui.press('home')
             if self.stop:
                 return
-            self.answer_question(driver)
-            if self.stop:
-                return
-            self.update_question_status_after_answering(question['id'])
+            if self.answer_question(driver):
+                self.update_question_status_after_answering(question['id'])
+                self.release_account()
+                if self.stop:
+                    return
+                time.sleep(self.page_refresh)
+                return self.start()
             if self.stop:
                 return
             time.sleep(self.page_refresh)
     
     def answer_question(self, driver: uc.Chrome):
-        time.sleep(5)
-        driver.find_element('xpath', '//div[contains(@class, "c-heading _questionContentsArea")]')
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        question_content = soup.select_one('div.c-heading._questionContentsArea')
-        question_content_cleaned = clean_question_content(question_content)
-        while True:
-            response = generate_response(question_content_cleaned)
-            if not text_has_prohibited_words(self.prohibited_words, response):
-                break
-            time.sleep(10)
-        finalized_text = pad_prescript_postscript(response)
-        bring_browser_to_front()
-        pyautogui.press('esc')
-        pyautogui.press('home')
-        pyperclip.copy(finalized_text)
-        bring_browser_to_front()
-        pyautogui.press('home')
-        pyautogui.hotkey('ctrl', 'v')
-        time.sleep(self.submit_delay)
-        register_answer_btn = driver.find_element('xpath', '//div[@id="smartEditorArea"]//div[@id="answerButtonArea"]//a[@id="answerRegisterButton"]')
-        driver.execute_script('arguments[0].click();', register_answer_btn)
-        self.handle_alerts(driver)
+        try:
+            time.sleep(5)
+            driver.find_element('xpath', '//div[contains(@class, "c-heading _questionContentsArea")]')
+            soup = BeautifulSoup(driver.page_source, 'lxml')
+            question_content = soup.select_one('div.c-heading._questionContentsArea')
+            question_content_cleaned = clean_question_content(question_content)
+            while True:
+                response = generate_response(question_content_cleaned)
+                if not text_has_prohibited_words(self.prohibited_words, response):
+                    break
+                time.sleep(10)
+            finalized_text = pad_prescript_postscript(response)
+            bring_browser_to_front()
+            pyautogui.press('esc')
+            pyautogui.press('home')
+            pyperclip.copy(finalized_text)
+            bring_browser_to_front()
+            pyautogui.press('home')
+            pyautogui.hotkey('ctrl', 'v')
+            time.sleep(self.submit_delay)
+            register_answer_btn = driver.find_element('xpath', '//div[@id="smartEditorArea"]//div[@id="answerButtonArea"]//a[@id="answerRegisterButton"]')
+            driver.execute_script('arguments[0].click();', register_answer_btn)
+            self.handle_alerts(driver)
+            return True
+        except Exception as e:
+            print(e)
+            return False
