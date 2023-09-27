4dfa2b50e41c3017499ada1dc69064799e6036c2
Daniel Bautista <eindal.bautista@gmail.com>
2023-09-27 09:49:13 +0800

    added reconnection functionality

--- networking/websocket_client.py
+++ networking/websocket_client.py
@@ -1,5 +1,6 @@
 import websockets
 import asyncio
+import json
 
 class WebsocketClient():
     def __init__(self, client_id, queues) -> None:
@@ -8,14 +9,22 @@ class WebsocketClient():
         self.ws_outbound = queues.ws_outbound
     
     async def receive_message(self, websocket):
-        while True:
-            inbound_msg = await websocket.recv()
-            await self.service_inbound.put(inbound_msg)
+        try:
+            while True:
+                inbound_msg = await websocket.recv()
+                if type(inbound_msg) is str:
+                    inbound_msg = json.loads(inbound_msg)
+                await self.service_inbound.put(inbound_msg)
+        except:
+            return await self.start()
     
     async def send_message(self, websocket):
-        while True:
-            outbound_msg = await self.ws_outbound.get()
-            await websocket.send_json(outbound_msg)
+        try:
+            while True:
+                outbound_msg = await self.ws_outbound.get()
+                await websocket.send(json.dumps(outbound_msg))
+        except:
+            return await self.start()
 
     async def start(self):
         async with websockets.connect(f"ws://localhost:8000/{self.client_id}") as websocket:
