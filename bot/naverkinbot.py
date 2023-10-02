caa0a73d5592d75fbd95817ee6e633d0aa866b5e
Daniel Bautista <eindal.bautista@gmail.com>
2023-10-02 16:06:02 +0800

    removed get, insert, update functions. can execute main() function and receive account, user_session, configs from server

--- bot/naverkinbot.py
+++ bot/naverkinbot.py
@@ -1,32 +1,37 @@
 from .async_worker import AsyncWorker
+import asyncio
 
 class NaverKinBot(AsyncWorker):
     def __init__(self, queues) -> None:
         super().__init__(queues)
+        self.account = {}
+        self.user_session = {}
+        self.configs = {}
         self.prev_account = ''
+        self.running = False
         self.stop = False
         self.on_error = False
     
     async def execute_task(self, task: str):
         if task == "START":
-            pass
+            if not self.running:
+                self.running = True
+                asyncio.create_task(self.main())
         elif task == "STOP":
             self.stop = True
     
-    async def get(self, table: str, column: str = "*", params: dict = {}):
-        table = table.replace(" ", "")
-        table = table.split(",")
-        column = column.replace(" ", "")
-        column = column.split(",")
-        await self.service_outbound.put({"query": "get", "data": {"table": table, "column": column, "params": params}})
-    
-    async def insert(self, table: str, column: str, values: str, params: dict = {}):
-        column = column.replace(" ", "")
-        column = column.split(",")
-        values = values.replace(" ", "")
-        values = values.split(",")
-        await self.service_outbound.put({"query": "insert", "data": {"table": table, "column": column, "values": values, "params": params}})
-    
-    async def update(self, table: str, column: dict, params: dict = {}):
-        await self.service_outbound.put({"query": "update", "data": {"table": table, "column": column, "params": params}})
+    async def main(self):
+        print("STARTED")
+
+        # WAIT FOR SERVER TO SEND USERNAME/PASSWORD
+        self.account = await self.data_queue.get()
+        print(f"{self.account['username']} LOGGED IN")
+
+        #WAIT FOR SERVER TO SEND USER SESSION
+        self.user_session = await self.data_queue.get()
+
+        #WAIT FOR SERVER TO SEND CONFIGS
+        self.configs = await self.data_queue.get()
+        print("END")
+        return
     
