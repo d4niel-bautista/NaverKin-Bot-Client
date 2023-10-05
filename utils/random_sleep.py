d6131e6ce2ff76d1653f6e3732afa3bbd14b2e2f
Daniel Bautista <eindal.bautista@gmail.com>
2023-10-05 09:42:24 +0800

    refactored

--- utils/random_sleep.py
+++ utils/random_sleep.py
@@ -3,8 +3,10 @@ import asyncio
 
 async def short_sleep(time):
     random.seed()
-    await asyncio.sleep(round(random.uniform(time, time + 10), 2))
+    sleep_time = round(random.uniform(time, time + 10), 2)
+    await asyncio.sleep(sleep_time)
 
 async def long_sleep(time):
     random.seed()
-    await asyncio.sleep(round(random.uniform(time + 2, time + 40), 2))
+    sleep_time = round(random.uniform(time + 2, time + 40), 2)
+    await asyncio.sleep(sleep_time)
