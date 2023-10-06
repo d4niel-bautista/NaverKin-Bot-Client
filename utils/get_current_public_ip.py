0b58b379ef7e4df6ab4eec09fbc8d1fc2df04382
Daniel Bautista <eindal.bautista@gmail.com>
2023-10-06 15:10:58 +0800

    refactored getting public ip function

--- utils/get_current_public_ip.py
+++ utils/get_current_public_ip.py
@@ -1,5 +1,5 @@
 import subprocess
 
 async def get_current_public_ip():
-    public_ip = subprocess.call("curl ifconfig.me")
+    public_ip = subprocess.Popen('''powershell -command "Invoke-RestMethod -Uri 'https://ifconfig.me' -UseBasicParsing"''', stdout=subprocess.PIPE).communicate()[0].decode().strip()
     return public_ip
