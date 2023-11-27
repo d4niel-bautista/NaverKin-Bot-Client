89eed2af0d5241f6fd69d2a3ced6355fe70e5a78
Daniel Bautista <eindal.bautista@gmail.com>
2023-11-27 16:30:47 +0800

    wrapped check_answer_registered inside try-block

--- utils/answer_registered.py
+++ utils/answer_registered.py
@@ -1,15 +1,17 @@
 import asyncio
 
 async def check_answer_registered(driver, question_link: str, account_url: str, handle_alerts):
-    if not account_url:
-        return False
-    driver.get(question_link)
-    await handle_alerts(driver)
-    await asyncio.sleep(5)
-    answer_list = driver.find_element('xpath', '//div[@class="answer-content__list _answerList"]')
     try:
+        if not account_url:
+            return False
+        driver.get(question_link)
+        await handle_alerts(driver)
+        await asyncio.sleep(5)
+        answer_list = driver.find_element('xpath', '//div[@class="answer-content__list _answerList"]')
         answer = answer_list.find_element('xpath', f'.//a[contains(@href, "{account_url}")]')
         if answer:
             return True
-    except:
+    except Exception as e:
+        print("error on check answer registered")
+        print(e)
         return False
