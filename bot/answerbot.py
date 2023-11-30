b577c67ca8288b9f8754247d52065a89ed7fb4ce
Daniel Bautista <eindal.bautista@gmail.com>
2023-11-30 14:58:20 +0800

    updated answerbot answer_question() function to implement checking if answer has registered, and will retry if answer is not registered. will save the answer response to the database and check if it is saved successfully, it has 3 attempts when checking

--- bot/answerbot.py
+++ bot/answerbot.py
@@ -1,9 +1,11 @@
 from bot import NaverKinBot
 import undetected_chromedriver as uc
-from utils import short_sleep, long_sleep, bring_browser_to_front
+from utils import short_sleep, long_sleep, bring_browser_to_front, check_answer_registered
 import pyperclip
 import pyautogui
-from networking.service import send_notification, update_account_interactions
+from networking.service import send_notification, update_account_interactions, save_answer_response, get_answer_response
+import asyncio
+from datetime import datetime
 
 class AnswerBot(NaverKinBot):
     def __init__(self, queues, mode: int) -> None:
@@ -30,28 +32,52 @@ class AnswerBot(NaverKinBot):
     async def answer_question(self, driver: uc.Chrome, question: dict, answer: str) -> bool:
         try:
             await short_sleep(5)
-            driver.get(question["question_link"] + '&mode=answer')
+            question_link = question["question_link"] + '&mode=answer'
+            driver.get(question_link)
             await bring_browser_to_front()
             pyautogui.press('esc')
             pyautogui.press('home')
             await short_sleep(30)
+            answer_content = ""
             if self.mode == 0:
-                pyperclip.copy(answer["answer_advertisement"])
+                answer_content = answer["answer_advertisement"]
+                answer_type = "answer_advertisement"
             elif self.mode == 1:
-                pyperclip.copy(answer["answer_exposure"])
+                answer_content = answer["answer_exposure"]
+                answer_type = "answer_exposure"
+            pyperclip.copy(answer_content)
             await bring_browser_to_front()
             pyautogui.press('home')
             pyautogui.hotkey('ctrl', 'v')
             await long_sleep(self.configs["submit_delay"])
-            register_answer_btn = driver.find_element('xpath', '//div[@id="smartEditorArea"]//div[@id="answerButtonArea"]//a[@id="answerRegisterButton"]')
-            driver.execute_script('arguments[0].click();', register_answer_btn)
-            await short_sleep(5)
             try:
-                driver.switch_to.alert.accept()
-            except Exception as e:
-                print(e)
-            await short_sleep(5)
-            return True
+                driver.execute_script("document.querySelector('#answerRegisterButton').click()")
+                await self.handle_alerts(driver=driver)
+            except:
+                print("can not click answer submit")
+
+            await short_sleep(10)
+            account_url = self.account["account_url"].split("naver.com")[-1]
+            if await check_answer_registered(driver=self.driver, question_link=question_link, account_url=account_url, handle_alerts=self.handle_alerts):
+                await save_answer_response(question_url=question_link, type=answer_type, content=answer_content, username=self.account["username"], postscript="", date_answered=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                save_attempts = 0
+                while True:
+                    if save_attempts >= 3:
+                        print("ERROR SAVING ANSWER RESPONSE TO DATABASE")
+                        break
+                    await asyncio.sleep(5)
+                    await get_answer_response(filters={"question_url": question_link, "username": self.account["username"], "type": answer_type})
+                    saved_answer_response = await self.data_queue.get()
+                    if saved_answer_response["question_url"] == question_link and saved_answer_response["username"] == self.account["username"] and saved_answer_response["type"] == answer_type:
+                        print("SAVED ANSWER RESPONSE TO DATABASE")
+                        break
+                    else:
+                        await save_answer_response(question_url=question_link, type=answer_type, content=answer_content, username=self.account["username"], postscript="", date_answered=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                        save_attempts += 1
+                return True
+            else:
+                print("ANSWER IS NOT REGISTERED")
+                return self.answer_question(driver=driver, question=question, answer=answer)
         except Exception as e:
             print("THERE IS ERROR WHILE ANSWERING")
             print(e)
