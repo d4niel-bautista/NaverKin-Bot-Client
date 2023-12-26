2c7294e3b5d4c7453ca90953566d38fa531bc66c
Daniel Bautista <eindal.bautista@gmail.com>
2023-12-26 12:53:23 +0800

    implemented VM id when starting autoanswerbot

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from networking.queues import ws_outbound, bot_client_inbound
 from networking.websocket_client import WebsocketClient
 import argparse
 import asyncio
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 async def start():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("-bt", help="Sets which bot type to use.\nInput 'aa' for AnswerBot_Advertisement, 'ae' for AnswerBot_Exposure, or 'q' for QuestionBot.", required=True, choices=['aa', 'ae', 'au', 'q'])
     parser.add_argument("-s", help="Sets the server address.", required=True)
     args = parser.parse_args()
+
+    VM_id = ""
     server = args.s
     if args.bt == 'aa':
         bot = AnswerBot(bot_client_inbound, mode=0)
@@ -19,10 +24,11 @@ async def start():
     elif args.bt == 'au':
         bot = AutoanswerBot(bot_client_inbound)
         client_id = "autoanswerbot"
+        VM_id = os.environ["VM_id"]
     elif args.bt == 'q':
         bot = QuestionBot(bot_client_inbound)
         client_id = "questionbot"
-    ws_client = WebsocketClient(server, client_id, bot_client_inbound, ws_outbound)
+    ws_client = WebsocketClient(server, client_id, bot_client_inbound, ws_outbound, VM_id=VM_id)
     await asyncio.gather(bot.start(), ws_client.start())
 
 asyncio.run(start())
