2ae2c6410ce7fee92bf8d9b4f6dcf738679e49d7
Daniel Bautista <eindal.bautista@gmail.com>
2024-01-03 12:23:44 +0800

    updated check_answer_registered() to keep clicking next page button element if it is visible before finding the answer element that matches the account_url, this will fix the error about answer not being registed but the answer is just not visible due to being in the next pages

--- utils/answer_registered.py
+++ utils/answer_registered.py
@@ -7,6 +7,15 @@ async def check_answer_registered(driver, question_link: str, account_url: str, 
         driver.default_get(question_link)
         await handle_alerts(driver)
         await asyncio.sleep(5)
+        while True:
+            next_page_btn = driver.find_elements('xpath', '//a[@id="nextPageButton"]')
+            if next_page_btn:
+                if next_page_btn[0].is_displayed():
+                    next_page_btn[0].uc_click()
+                    await asyncio.sleep(5)
+                    continue
+            await asyncio.sleep(5)
+            break
         answer_list = driver.find_element('xpath', '//div[@class="answer-content__list _answerList"]')
         answer = answer_list.find_element('xpath', f'.//a[contains(@href, "{account_url}")]')
         if answer:
